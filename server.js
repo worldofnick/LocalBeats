8d17a611482701f98db95aaeb2078aea80f74319
Nick Porter <worldofnick@users.noreply.github.com>
2017-11-23 17:48:00 -0700

    more routes

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var ObjectID = mongodb.ObjectID;
 
 var USERS_COLLECTION = "users";
 var EVENT_COLLECTION = "events";
+var BOOKING_COLLECTION = "bookings";
 
 var app = express();
 app.use(bodyParser.json());
@@ -40,8 +41,6 @@ function handleError(res, reason, message, code) {
   res.status(code || 500).json({"error": message});
 }
 
-
-
 /*  "/api/profile"
  *    GET: gets a user profile info by an id
  *    POST: creates a new profile
@@ -49,14 +48,15 @@ function handleError(res, reason, message, code) {
  *    DELETE: delets a users profile by an id
  */
  app.get("/api/user/", function(req, res) {
-   res.status(200).json(req.body.user.uid)
    db.collection(USERS_COLLECTION).findOne({ _id: new ObjectID(req.body.user.uid) }, function(err, doc) {
      if (err) {
        handleError(res, err.message, "Failed to get user");
      } else {
+       db.collection(EVENT_COLLECTION).deleteMany({ uid : req.body.user.uid }) // Also delete all events owned by this user
        res.status(200).json(doc);
      }
    });
+
  });
 
  app.post("/api/user", function(req, res) {
@@ -74,7 +74,7 @@ function handleError(res, reason, message, code) {
    var updateDoc = req.body;
    delete updateDoc._id;
 
-   db.collection(USERS_COLLECTION).updateOne({_id: new ObjectID(req.body.user.uid)}, updateDoc, function(err, doc) {
+   db.collection(USERS_COLLECTION).updateOne({uid: req.body.user.uid}, updateDoc, function(err, doc) {
      if (err) {
        handleError(res, err.message, "Failed to update user");
      } else {
@@ -85,7 +85,7 @@ function handleError(res, reason, message, code) {
  });
 
  app.delete("/api/user/", function(req, res) {
-   db.collection(USERS_COLLECTION).deleteOne({_id: new ObjectID(req.body.user.uid)}, function(err, result) {
+   db.collection(USERS_COLLECTION).deleteOne({ uid: req.body.user.uid }, function(err, result) {
      if (err) {
        handleError(res, err.message, "Failed to delete user");
      } else {
@@ -101,15 +101,110 @@ function handleError(res, reason, message, code) {
   *    PUT: updates an event by id
   *    DELETE: delets an event by id
   */
+  // Get all events owned by a user
+  // Get all events in geo space
+  // Get all events matching some filter
 
   app.get("/api/events", function(req, res) {
+    db.collection(EVENT_COLLECTION).findOne({ eid: req.body.event.eid }, function(err, doc) {
+      if (err) {
+        handleError(res, err.message, "Failed to get event");
+      } else {
+        res.status(200).json(doc);
+      }
+    });
   });
 
   app.post("/api/events", function(req, res) {
+    var newEvent = req.body;
+    db.collection(EVENT_COLLECTION).insertOne(newEvent, function(err, doc) {
+      if (err) {
+        handleError(res, err.message, "Failed to create new event.");
+      } else {
+        res.status(201).json(doc.ops[0]);
+      }
+    });
   });
 
   app.put("/api/events", function(req, res) {
+    var updateDoc = req.body;
+    delete updateDoc._id;
+
+    db.collection(EVENT_COLLECTION).updateOne({eid: req.body.event.eid}, updateDoc, function(err, doc) {
+      if (err) {
+        handleError(res, err.message, "Failed to update event");
+      } else {
+        updateDoc._id = req.params.id;
+        res.status(200).json(updateDoc);
+      }
+    });
   });
 
   app.delete("/api/events", function(req, res) {
+    db.collection(EVENT_COLLECTION).deleteOne({ eid: req.body.event.eid }, function(err, result) {
+      if (err) {
+        handleError(res, err.message, "Failed to delete event");
+      } else {
+        // Delete all bookings for this event?
+        res.status(200).json(req.params.id);
+      }
+    });
+  });
+
+  app.get("/api/userEvents", function(req, res) {
+    db.collection(EVENT_COLLECTION).find({ uid: req.body.event.uid }, function(err, doc) {
+      if (err) {
+        handleError(res, err.message, "Failed to get event");
+      } else {
+        res.status(200).json(doc);
+      }
+    });
+  });
+
+
+  // BOOKINGS
+  app.get("/api/bookings", function(req, res) {
+    db.collection(BOOKING_COLLECTION).findOne({ bid: req.body.booking.bid }, function(err, doc) {
+      if (err) {
+        handleError(res, err.message, "Failed to get event");
+      } else {
+        res.status(200).json(doc);
+      }
+    });
+  });
+
+  app.post("/api/bookings", function(req, res) {
+    var newEvent = req.body;
+    db.collection(BOOKING_COLLECTION).insertOne(newEvent, function(err, doc) {
+      if (err) {
+        handleError(res, err.message, "Failed to create new event.");
+      } else {
+        res.status(201).json(doc.ops[0]);
+      }
+    });
+  });
+
+  app.put("/api/bookings", function(req, res) {
+    var updateDoc = req.body;
+    delete updateDoc._id;
+
+    db.collection(BOOKING_COLLECTION).updateOne({ bid: req.body.booking.bid }, updateDoc, function(err, doc) {
+      if (err) {
+        handleError(res, err.message, "Failed to update event");
+      } else {
+        updateDoc._id = req.params.id;
+        res.status(200).json(updateDoc);
+      }
+    });
+  });
+
+  app.delete("/api/bookings", function(req, res) {
+    db.collection(BOOKING_COLLECTION).deleteOne({ bid: req.body.booking.bid }, function(err, result) {
+      if (err) {
+        handleError(res, err.message, "Failed to delete event");
+      } else {
+        // Delete all bookings for this event?
+        res.status(200).json(req.params.id);
+      }
+    });
   });
