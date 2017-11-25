3c0a66471ddd54e16e08de15dbb30cb05ca82cc8
Brandon Koch <brandon.gregory.koch@gmail.com>
2017-11-25 15:07:20 -0700

    Readded in services and models

--- src/app/profile/profile-edit/profile-edit.component.ts
+++ src/app/profile/profile-edit/profile-edit.component.ts
@@ -1,20 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { User } from 'app/user/user';
+import { User } from 'app/models/user';
 import { Router } from '@angular/router';
 @Component({
   selector: 'app-profile-edit',
   templateUrl: './profile-edit.component.html',
   styleUrls: ['./profile-edit.component.css']
 })
-
-
-// User: 
-// token: string;
-// first_name: string;
-// last_name: string;
-// email: string;
-// phone: string;
-// password: string;
 
 
 export class ProfileEditComponent implements OnInit {
