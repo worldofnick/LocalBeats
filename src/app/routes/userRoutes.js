b5cd5aa6ace7281778b48db8f928b3fd06445159
ash-19 <u0946268@utah.edu>
2017-11-26 11:57:17 -0700

    Now contains only userRoutes

--- src/app/routes/userRoutes.js
+++ src/app/routes/userRoutes.js
@@ -2,8 +2,7 @@
 
 module.exports = function(app) {
 	var userHandlers = require('../controllers/userController.js');
-	var tokenVerificationHandler = require('../controllers/tokenVerificationController.js');
-	var autheticationHandler = require('../controllers/authenticationController.js');
+	// var tokenVerificationHandler = require('../controllers/tokenVerificationController.js');
 
 	// Users routes
 	app.route('/api/users')
@@ -14,22 +13,4 @@ module.exports = function(app) {
 		.get(userHandlers.getUserByID)
 		.put(userHandlers.updateUserByID)
 		.delete(userHandlers.deleteUserByID);
-
-	// Authentication Routes
-	app.route('/api/auth/register')
-		.post(autheticationHandler.register);
-
-	app.route('/api/auth/authenticate')
-		.post(autheticationHandler.signIn);
-
-	app.route('/api/auth/logout')
-		.get(autheticationHandler.logout);
-
-	/**
-	 * Authorization - the act of verifying the access rights of a user to 
-	 * interact with a resource. Can't be accessed without valid JWT.
-	 * Need to pass JWT token in x-access-token header in the GET request
-	 */
-	app.route('/api/auth/whoami')
-		.get(tokenVerificationHandler.verifyToken, autheticationHandler.whoAmI);
 };
