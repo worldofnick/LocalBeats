6a5db3c6136c20652c90da2f90d332bb9b944569
ash-19 <u0946268@utah.edu>
2017-11-26 11:49:52 -0700

    User MongoDB schema
    - Is strict, that is, any payload besides the ones in the schema will
    NOT be added to the DB.
    - TODO: replace the dummy with refined schema

--- src/app/models/userModel.js
+++ src/app/models/userModel.js
@@ -2,14 +2,38 @@ var mongoose = require('mongoose');
 var bcrypt = require('bcrypt');
 var Schema = mongoose.Schema;
 
+
 /**
- * set up a mongoose model
- * email is unique
- */ 
-module.exports = mongoose.model('Users', new Schema({ 
-    first_name: {
-        String,
-	email: String, 
-	password: String, 
-	admin: Boolean 
-}));
+ * User Schema
+ */
+var UserSchema = new Schema({
+    // name: {
+    //     tyep:String,
+    //     required: true
+    // },
+    // email: {
+    //   type: String,
+    //   unique: true,
+    //   lowercase: true,
+    //   trim: true,
+    //   required: true
+    // },
+    // hash_password: {
+    //   type: String,
+    //   required: true
+    // },
+    // created: {
+    //   type: Date,
+    //   default: Date.now
+    // }
+    name: String,
+    email: String,
+    hash_password: String,
+    created: {type: Date, default: Date.now}
+}, {strict: true});
+  
+UserSchema.methods.comparePassword = function(password) {
+    return bcrypt.compareSync(password, this.hash_password);
+};
+
+mongoose.model('User', UserSchema);
