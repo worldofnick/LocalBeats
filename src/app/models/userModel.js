d8ab19274840038d3c7b4e199ea0e75378f3414a
ash-19 <u0946268@utah.edu>
2017-11-26 13:20:02 -0700

    Updated the User model

--- src/app/models/userModel.js
+++ src/app/models/userModel.js
@@ -6,32 +6,24 @@ var Schema = mongoose.Schema;
 /**
  * User Schema
  */
+//TODO: add match() validator to email
 var UserSchema = new Schema({
-    // name: {
-    //     tyep:String,
-    //     required: true
-    // },
-    // email: {
-    //   type: String,
-    //   unique: true,
-    //   lowercase: true,
-    //   trim: true,
-    //   required: true
-    // },
-    // hash_password: {
-    //   type: String,
-    //   required: true
-    // },
-    // created: {
-    //   type: Date,
-    //   default: Date.now
-    // }
-    name: String,
-    email: String,
-    hash_password: String,
-    created: {type: Date, default: Date.now}
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true},
+    email: {type: String, unique: true, lowercase: true, required: true},
+    hashPassword: {type: String, required: true},
+    borthday: {type: Date, required: true},
+    joinDate: {type: Date, default: Date.now},
+    profilePicID: {type: Number},
+    soundcloudID: {type: String},
+    spotifyID: {type: String},
+    likesGenres: {tyep: Array}
 }, {strict: true});
-  
+
+/**
+ * Compares the passed password to the hashed password in the 
+ * DB and return the result
+ */ 
 UserSchema.methods.comparePassword = function(password) {
     return bcrypt.compareSync(password, this.hash_password);
 };
