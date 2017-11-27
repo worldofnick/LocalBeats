e17bcf63fb0e59b3535cb85a764c14767142930b
Nick Porter <worldofnick@users.noreply.github.com>
2017-11-26 19:08:08 -0700

    update schema

--- src/app/models/bookingsModel.js
+++ src/app/models/bookingsModel.js
@@ -8,18 +8,7 @@ var Schema = mongoose.Schema;
 
 // TODO
 var BookingsSchema = new Schema({
-    eventName       : {type: String, required: true},
-    hostUID         : {type: String, required: true},
-    hostEmail       : {type: String, lowercase: true, required: true},
-    performerEmail  : {type: String, lowercase: true, required: true},
-    address         : {type: String},
-    fromDate        : {type: Date, default: Date.now},
-    toDate          : {type: Date},
-    description     : {type: String},
-    fixedPrice      : {type: Number},
-    hourlyRate      : {type: Number},
-    deposit         : {type: Number},
-    isBooked          : {type: Boolean}
+    hostUID         : {type: String, required: true}
 }, {strict: true});
 
 mongoose.model('Bookings', BookingsSchema);
