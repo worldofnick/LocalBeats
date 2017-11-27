2ac645590fd3a69c41633dab9c3c492ae4520079
Nick Porter <worldofnick@users.noreply.github.com>
2017-11-26 18:39:08 -0700

    Add booking controller

---
+++ src/app/controllers/bookingsController.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var mongoose    = require('mongoose');
+var jwt         = require('jsonwebtoken');
+var bcrypt      = require('bcrypt');
+var Bookings      = mongoose.model('Bookings');
+var config      = require('../../../config.js');
+
+// ====== Bookings ROUTES ======
+
+exports.getBookingByID = function (req, res) {
+  Bookings.findById(req.params.bid, function (err, booking) {
+      if (err) {
+          return res.status(500).send("Failed to get booking");
+      } else {
+          return res.status(200).send(booking);
+      }
+  });
+};
+
+exports.createBooking = function (req, res) {
+    var newBooking = new Bookings(req.body);
+    newBooking.save(function (err, booking) {
+        if (err) {
+            return res.status(400).send({
+                message: err,
+                description: "Failed to create a booking"
+            });
+        } else {
+            return res.status(200).send(booking);
+        }
+    });
+};
+
+exports.updateBookingByID = function (req, res) {
+    Bookings.findByIdAndUpdate(req.params.bid, req.body, { new: true }, function (err, booking) {
+        if (err) {
+            return res.status(500).send("There was a problem updating the booking.");
+        }
+        return res.status(200).send(booking);
+    });
+};
+
+exports.deleteBookingByID = function (req, res) {
+    Bookings.findByIdAndRemove(req.params.bid, function (err, booking) {
+        if (err) {
+            return res.status(500).send("There was a problem deleting the booking.");
+        } else {
+            if (booking == null) {
+                return res.status(200).send("Booking was already deleted.");
+            } else {
+                return res.status(200).send("Booking removed");
+            }
+        }
+    });
+};
