9bf3397e24359111732f2c936ba9854a4d96d249
Nick Porter <worldofnick@users.noreply.github.com>
2017-11-27 13:12:37 -0700

    error

--- src/app/controllers/eventsController.js
+++ src/app/controllers/eventsController.js
@@ -183,6 +183,7 @@ exports.searchEvents = function(req, res) {
 
   Events.find(query).limit(limit).skip(skip).exec(function (err, doc) {
       if (err) {
+          console.log(err);
           return res.status(500).send("Failed to get user events");
       } else {
           return res.status(200).send(doc);
