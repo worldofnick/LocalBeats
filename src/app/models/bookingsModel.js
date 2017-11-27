8eb4dca5887bc4f1d88e8d2157663dd17f19b3f2
Nick Porter <worldofnick@users.noreply.github.com>
2017-11-27 11:05:14 -0700

    update schema

--- src/app/models/bookingsModel.js
+++ src/app/models/bookingsModel.js
@@ -10,7 +10,10 @@ var Schema = mongoose.Schema;
 var BookingsSchema = new Schema({
     hostUID         : {type: String, required: true},
     performerUID    : {type: String, required: true},
-    eventEID        : {type: String, required: true}
+    eventEID        : {type: String, required: true},
+    fromDate        : {type: Date, default: Date.now},
+    toDate          : {type: Date},
+    completed       : {type: Boolean, default: false} // Has the event had the 2 factor auth go through?
 }, {strict: true});
 
 mongoose.model('Bookings', BookingsSchema);
