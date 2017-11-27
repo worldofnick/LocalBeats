b269f803cdb77afae4f77013929d15dbdd73b0b6
Nick Porter <worldofnick@users.noreply.github.com>
2017-11-27 14:21:22 -0700

    repair user routes

--- src/app/controllers/userController.js
+++ src/app/controllers/userController.js
@@ -9,16 +9,16 @@ var config    = require('../../../config.js');
 // ====== USER ROUTES ======
 
 exports.listAllUsers = function (req, res) {
-  User.find({}, { hash_password: 0 }, function (err, user) {
+  User.find({}, { hashPassword: 0 }, function (err, user) {
     if (err)
       return res.send(err);
-    user.hash_password = undefined;
+    user.hashPassword = undefined;
     return res.status(200).send(user);
   });
 };
 
 exports.getUserByID = function (req, res) {
-  User.findById(req.params.uid, { hash_password: 0 }, function (err, user) {
+  User.findById(req.params.uid, { hashPassword: 0 }, function (err, user) {
     if (err)
       return res.send(err);
     return res.json(user);
@@ -26,10 +26,10 @@ exports.getUserByID = function (req, res) {
 };
 
 exports.updateUserByID = function (req, res) {
-  User.findByIdAndUpdate(req.params.uid, req.body, { new: true }, function (err, user) {
+  User.findByIdAndUpdate(req.params.uid, req.user.body, { new: true }, function (err, user) {
     if (err) return res.status(500).send("There was a problem updating the user.");
 
-    user.hash_password = undefined;
+    user.hashPassword = undefined;
     return res.status(200).send(user);
   });
 };
@@ -43,8 +43,23 @@ exports.deleteUserByID = function (req, res) {
       if (user == null) {
         return res.status(200).send("User was already deleted.");
       } else {
-        return res.status(200).send("User " + user.name + " is removed");
+        return res.status(200).send("User " + user.email + " is removed");
       }
     }
   });
 };
+
+// exports.searchUsersByName = function (req, res) {
+//
+//   var query = {};
+//   if (req.query.name != null) {
+//
+//   }
+//
+//   User.find(query, { hashPassword: 0 }, function (err, user) {
+//     if (err)
+//       return res.send(err);
+//     user.hashPassword = undefined;
+//     return res.status(200).send(user);
+//   });
+// };
