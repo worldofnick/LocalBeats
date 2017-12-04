dce0e0a74657d1973b0b460735029e3c44cce5f6
Nick Porter <worldofnick@users.noreply.github.com>
2017-12-04 15:56:43 -0700

    get user bookigns update

--- backend/controllers/bookingsController.js
+++ backend/controllers/bookingsController.js
@@ -64,20 +64,61 @@ exports.deleteBookingByID = function (req, res) {
     });
 };
 
+// Params 
+// uid (string) user id in question --requied
+// user_type (string) in {"arist", "host"} --required
+// status (string) in {"approved", "pending"} --optional - defaults to all
+// booking_type (string) in {"arist-apply", "host-request"} --optional defaults to all
+// eid (string) event ID in question --optional
+
+// skip (int) how many records to skip --optional - defaults to 0
+// limit (int) how many records to return --optional - defaults to 10
+
+// Get a list of events an artist has applied for
+// Get a list of hosts requested artists (for a given event)
+// Get a list of all bookings for a user
+// Get a list of all approved events for an artist
+
 exports.getUserBookingsByUID = function (req, res) {
   var limit = 10;
   var skip = 0;
+  var query = {};
 
   if (req.params.limit != null) {
       limit = parseInt(req.query.limit);
-
   }
 
   if (req.params.skip != null) {
       skip = parseInt(req.query.skip);
   }
 
-  Bookings.find({hostUID: req.query.hostUID}).limit(limit).skip(skip).exec(function (err, doc) {
+  if (req.params.user_type == null) {
+    return res.status(200).send({"error": "Must send user_type"});
+  } else {
+      if (req.params.user_type == "artist") {
+        query.performerUID = req.params.uid;
+      } else if (req.params.user_type == "host") {
+        query.hostUID = req.params.uid;
+      }
+  }
+
+  if (req.params.status != null) {
+    if (req.params.status == "approved") {
+        query.approved = true;
+    } else if (req.params.status == "pending") {
+        query.approved = false;
+    }
+  }
+
+  if (req.params.booking_type != null) {
+      query.bookingType = req.params.booking_type;
+  }
+
+  if (req.params.eid != null) {
+      query.eventEID = req.params.eid;
+  }
+
+  Bookings.find(query).limit(limit).skip(skip).sort({ fromDate: -1 }).exec(function (err, doc) {
       if (err) {
           return res.status(500).send("Failed to get user bookings");
       } else {
