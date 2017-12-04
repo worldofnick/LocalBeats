08613e545b202184776b8ade5e9503bf10eb5836
Nick Porter <worldofnick@users.noreply.github.com>
2017-12-04 15:56:26 -0700

    add performerUID and approved flag

--- backend/models/bookingsModel.js
+++ backend/models/bookingsModel.js
@@ -8,11 +8,13 @@ var Schema = mongoose.Schema;
 
 // TODO
 var BookingsSchema = new Schema({
-    hostUID         : {type: String, required: true},
-    performerUID    : {type: String, required: true},
+    bookingType     : {type: String, required: true}, // in {"arist-apply", "host-request"}
+    hostUID         : {type: String, required: false},
+    performerUID    : {type: String, required: false},
     eventEID        : {type: String, required: true},
     fromDate        : {type: Date, default: Date.now},
     toDate          : {type: Date},
+    approved        : {type: Boolean, default: false},
     completed       : {type: Boolean, default: false} // Has the event had the 2 factor auth go through?
 }, {strict: true});
 
