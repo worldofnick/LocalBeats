ffb40b60bcb658e8181d1e2362cca11f3394d087
Adam Rosenberg <adam.arosenberg@gmail.com>
2017-12-05 17:04:26 -0700

    pushing to merge for events

---
+++ src/app/events/my-events/my-events.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from 'app/models/user';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms/src/directives/ng_form';
+import { UserService } from 'app/services/user.service';
+import { EventService } from 'app/services/event.service';
+import { print } from 'util';
+import { Injectable } from '@angular/core';
+import { Event } from 'app/models/event';
+
+@Component({
+  selector: 'app-my-events',
+  templateUrl: './my-events.component.html',
+  styleUrls: ['./my-events.component.css']
+})
+export class MyEventsComponent implements OnInit {
+
+  user:User;
+  events:Event[];
+
+  constructor(private eventService: EventService, private userService: UserService, private router: Router) { }
+  
+  ngOnInit() {
+    this.user = this.userService.user;
+    
+    this.eventService.getEventsByUID(this.user).then((events: Event[]) => {
+      this.events = events;      
+      console.log("in nginit...events received:");
+      console.log(this.events); 
+    });
+  }
+
+}
