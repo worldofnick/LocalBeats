aaaf8f3c8854fd63ea49e688d5ea1ecaa70d3dd6
Nick Porter <worldofnick@users.noreply.github.com>
2017-12-05 22:56:59 -0700

    bookings encapsulate

--- backend/controllers/bookingsController.js
+++ backend/controllers/bookingsController.js
@@ -19,7 +19,7 @@ exports.listAllBookings = function (req, res) {
             bkkins.push({"booking": booking});
         });
             
-        return res.status(200).send(bkkins);
+        return res.status(200).send({"bookings": bkkins});
     });
 };
 
@@ -141,7 +141,7 @@ exports.getUserBookingsByUID = function (req, res) {
                 bkkins.push({"booking": booking});
             });
                 
-            return res.status(200).send(bkkins);
+            return res.status(200).send({"bookings": bkkins});
       }
   });
 };
