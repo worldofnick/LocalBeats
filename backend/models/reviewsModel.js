17442eede2ae18b269938296de538a11c5f47ad2
Nick Porter <worldofnick@users.noreply.github.com>
2017-12-08 00:14:28 -0700

    Join on review

--- backend/models/reviewsModel.js
+++ backend/models/reviewsModel.js
@@ -6,11 +6,13 @@ var Schema = mongoose.Schema;
  * Review Schema
  */
 var ReviewSchema = new Schema({
-    toUID           : {type: String, required: true},
-    fromUID         : {type: String, required: true},
     title           : {type: String, required: true},
     text            : {type: String, required: true},
     rating          : {type: Number, required: true},
+    fromUser        : {type: String, required: true},
+    toUser          : {type: Schema.Types.ObjectId, ref: 'User' },
+    toUser          : {type: Schema.Types.ObjectId, ref: 'User' },
+    event           : {type: Schema.Types.ObjectId, ref: 'Events' },
     date            : {type: Date, default: Date.now},
     flagCount       : {type: Number, default: 0}
 }, {strict: true}, {versionKey: false});
