69e9d8f5a9f4919382fb78666aacfdeac0e798bf
Brandon Koch <brandon.gregory.koch@gmail.com>
2018-01-14 00:03:27 -0700

    signin is working

--- src/app/views/auth/auth.component.ts
+++ src/app/views/auth/auth.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatProgressBar, MatButton } from '@angular/material';
 import { Validators, FormGroup, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from '../../services/auth/user.service';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-auth',
@@ -11,8 +14,9 @@ export class AuthComponent implements OnInit {
   @ViewChild(MatButton) submitButton: MatButton;
 
   signinForm: FormGroup;
+  user: User;
 
-  constructor() { }
+  constructor(private userService: UserService, private router: Router) { }
 
   ngOnInit() {
     this.signinForm = new FormGroup({
@@ -24,10 +28,25 @@ export class AuthComponent implements OnInit {
 
   signin() {
     const signinData = this.signinForm.value
-    console.log(signinData);
+    this.user = {
+      _id: null,
+      firstName: null,
+      lastName: null,
+      email: signinData.username,
+      password: signinData.password,
+      spotifyID: null,
+      genres: [],
+      isArtist: true
+    };
 
     this.submitButton.disabled = true;
     this.progressBar.mode = 'indeterminate';
+
+    this.userService.signinUser(this.user).then((user: User) => {
+      this.user = user;
+      this.router.navigate(['/']);
+    });
+
   }
 
 }
