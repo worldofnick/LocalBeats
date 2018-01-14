7406ddbf10f281e5eb42b686c7e8032bf4fb6d37
Brandon Koch <brandon.gregory.koch@gmail.com>
2018-01-14 00:19:09 -0700

    Registration working

--- src/app/views/auth/register/register.component.ts
+++ src/app/views/auth/register/register.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatProgressBar, MatButton } from '@angular/material';
 import { Validators, FormGroup, FormControl } from '@angular/forms';
 import { CustomValidators } from 'ng2-validation';
+import { Router } from '@angular/router';
+import { UserService } from '../../../services/auth/user.service';
+import { User } from '../../../models/user';
 
 @Component({
   selector: 'app-register',
@@ -11,17 +14,21 @@ export class RegisterComponent implements OnInit {
   @ViewChild(MatProgressBar) progressBar: MatProgressBar;
   @ViewChild(MatButton) submitButton: MatButton;
 
-  signupForm: FormGroup
-  constructor() {}
+  signupForm: FormGroup;
+  user: User;
+
+  constructor(private userService: UserService, private router: Router) {}
 
   ngOnInit() {
     const password = new FormControl('', Validators.required);
     const confirmPassword = new FormControl('', CustomValidators.equalTo(password));
 
     this.signupForm = new FormGroup({
+      firstName: new FormControl('', Validators.required),
+      lastName: new FormControl('', Validators.required),
       email: new FormControl('', [Validators.required, Validators.email]),
       password: password,
-      confirmPassword: confirmPassword,
+      confirmPassword: confirmPassword, 
       agreed: new FormControl('', (control: FormControl) => {
         const agreed = control.value;
         if(!agreed) {
@@ -35,9 +42,25 @@ export class RegisterComponent implements OnInit {
   signup() {
     const signupData = this.signupForm.value;
     console.log(signupData);
+    this.user = {
+      _id: null,
+      firstName: signupData.firstName,
+      lastName: signupData.lastName,
+      email: signupData.email,
+      password: signupData.password,
+      spotifyID: null,
+      genres: [],
+      isArtist: true
+    };
+    console.log(this.user);
 
     this.submitButton.disabled = true;
     this.progressBar.mode = 'indeterminate';
+
+    this.userService.signupUser(this.user).then((user: User) => {
+      this.user = user;
+      this.router.navigate(['/']);
+    });
   }
 
 }
