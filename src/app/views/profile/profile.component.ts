b9a1b864268dd65ed9e34e353f01912f752b42af
Brandon Koch <brandon.gregory.koch@gmail.com>
2018-01-26 14:42:12 -0700

    Added create event button on top bar

--- src/app/views/profile/profile.component.ts
+++ src/app/views/profile/profile.component.ts
@@ -29,38 +29,6 @@ export class ProfileComponent implements OnInit {
   deleteStatus:Number;
   hasApplied:Boolean = true;
 
-  // Doughnut
-  doughnutChartColors: any[] = [{
-    backgroundColor: ['#fff', 'rgba(0, 0, 0, .24)',]
-  }];
-
-  total1: number = 500;
-  data1: number = 200;
-  doughnutChartData1: number[] = [this.data1, (this.total1 - this.data1)];
-
-  total2: number = 1000;
-  data2: number = 400;
-  doughnutChartData2: number[] = [this.data2, (this.total2 - this.data2)];
-
-  doughnutChartType = 'doughnut';
-  doughnutOptions: any = {
-    cutoutPercentage: 85,
-    responsive: true,
-    maintainAspectRatio: true,
-    legend: {
-      display: false,
-      position: 'bottom'
-    },
-    elements: {
-      arc: {
-        borderWidth: 0,
-      }
-    },
-    tooltips: {
-      enabled: false
-    }
-  };
-
   constructor(private route: ActivatedRoute,
     private userService: UserService,
     private bookingService: BookingService,
