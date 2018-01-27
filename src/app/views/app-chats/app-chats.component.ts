6e5da0bd9eda6b105447758b03b3b2006dfcc1cf
ash-19 <u0946268@utah.edu>
2018-01-27 15:25:58 -0700

    Added basic connection to SendBird Servers

--- src/app/views/app-chats/app-chats.component.ts
+++ src/app/views/app-chats/app-chats.component.ts
@@ -53,6 +53,9 @@ export class AppChatsComponent implements OnInit {
 
   ngOnInit() {
     this.chatSideBarInit();
+
+    this.startSendBird('Pikachu', 'Pikachu')    
+
   }
   changeActiveUser(user) {
     this.activeChatUser = user;
@@ -72,6 +75,27 @@ export class AppChatsComponent implements OnInit {
     this.screenSizeWatcher = this.media.subscribe((change: MediaChange) => {
       this.isMobile = (change.mqAlias == 'xs') || (change.mqAlias == 'sm');
       this.updateSidenav();
+    });
+  }
+
+  /***********************************************
+  *            SendBird Settings
+  **********************************************/
+  sb: any;
+  appId:string = '9DA1B1F4-0BE6-4DA8-82C5-2E81DAB56F23';
+
+  startSendBird(userId, nickName) {
+    this.sb = new SendBird({
+      appId: this.appId
+    });
+  
+    this.sb.connect(userId, function(user, error){
+      if (error) {
+        console.log('Failed to connect to the SendBird Servers: ' + error)
+        return;
+      } else {
+        console.log('Connected to the SendBird Servers: ' + user)
+      }
     });
   }
 }
