a610feafe5bd87e070d4209d57cddab79a6e3e18
ash-19 <u0946268@utah.edu>
2018-01-29 05:46:47 -0700

    connectedUsers for chatting

--- src/app/services/chats/chats.service.ts
+++ src/app/services/chats/chats.service.ts
@@ -2,16 +2,38 @@ import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs/Subject';
 import { Observable } from 'rxjs/Observable';
 import * as io from 'socket.io-client';
+import { User } from '../../models/user';
+import { UserService } from '../../services/auth/user.service';
 
 @Injectable()
 export class ChatsService {
 
   private url = 'http://localhost:8080';
   private socket = io(this.url);
+  private loggedInUser: User = new User();
 
-  constructor() { }
+  constructor(private _userService: UserService) { }
 
   getConnectionUsers() {
+    let users: User[] = new Array();
+    let user1 = new User();
+    let user2 = new User();
+    let user3 = new User();
+
+    this._userService.getUserByID('5a6f136cd9847106668772c1').then((gottenUser: User) => {
+      user1 = gottenUser;
+      users.push(user1);
+    });
+    this._userService.getUserByID('5a6f1348d9847106668772c0').then((gottenUser: User) => {
+      user2 = gottenUser;
+      users.push(user2);
+    });
+    this._userService.getUserByID('5a6cf410a502610f0771a16c').then((gottenUser: User) => {
+      user3 = gottenUser;
+      users.push(user3);
+    });
+
+    // return users;
     return [{
       name: 'Gevorg Spartak',
       photo: 'assets/images/face-2.jpg',
@@ -38,6 +60,11 @@ export class ChatsService {
       isOnline: true,
       lastMsg: 'We\'ll talk later'
     }];
+  }
+
+  getCurrentLoggedInUser() {
+    this.loggedInUser = this._userService.user;
+    return this.loggedInUser;
   }
 
   // TODO: add method join / switch user. Should send JoinPM with sender user and receiever user. Socekt can join 
