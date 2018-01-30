4ce66d343c7e33f05a06a07a4af6b18eeb8817a6
ash-19 <u0946268@utah.edu>
2018-01-30 13:22:47 -0700

    Added PM server routes

---
+++ backend/routes/messageRoutes.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = function(app) {
+	var messageHandlers = require('../controllers/messageController.js');
+    
+    // Message routes
+	app.route('/api/messages/from/:uid/to/:uid')        // SAME AS (to, from) == (from, to)
+		.get(messageHandlers.getAllFromToMessages);     // Get all messages from a user to a user
+
+	app.route('/api/messages/')
+		.put(messageHandlers.saveMessage)               // save message to DB
+        .delete(messageHandlers.clearMessagesDB);       // remove all messages from DB
+    
+    app.route('/api/messages/:uid')
+        .delete(messageHandlers.removeAllConversationsWithAUser);
+};
