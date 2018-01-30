481ecb28175f12459f860e97dd609a731779d318
ash-19 <u0946268@utah.edu>
2018-01-30 11:13:15 -0700

    Updated socket models

--- src/app/services/chats/model/event.ts
+++ src/app/services/chats/model/event.ts
@@ -4,5 +4,6 @@ export enum Event {
     NEW_LOG_IN = 'newUserLoggedIn',
     SMN_LOGGED_OUT = 'someUserLoggedOut',
     SEND_PRIVATE_MSG = 'sendPrivateMessage',
-    SEND_MSG = 'sendMessage'
+    SEND_MSG = 'sendMessage',
+    REQUEST_PM_SOCKET_ID = 'requestSocketIdForPM'
 }
