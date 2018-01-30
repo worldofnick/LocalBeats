659f5708917427ae255333e8d0fb1930653ee412
Brandon Koch <brandon.gregory.koch@gmail.com>
2018-01-30 16:30:44 -0700

    Created ainitial UI design for recommendations on the home component.  Logic of retrieving the recommendations remains.  Currently, just mock data is provided to demonstrate the UI.

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -6,7 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  artists = [{
+    name: 'Featured Drummer',
+    url: 'assets/images/drums-image.png'
+  }, {
+    name: 'Featured Trumpet',
+    url: 'assets/images/trumpet-pic.png'
+  }, {
+    name: 'Featured Guitarist',
+    url: 'assets/images/guitar-pic.jpg'
+  }]
 
+  events = [{
+    name: 'Featured Restaurant',
+    url: 'assets/images/coffee-shop-pic.jpg'
+  }, {
+    name: 'Featured Concert',
+    url: 'assets/images/concert-pic.jpeg'
+  }, {
+    name: 'Featured Wedding',
+    url: 'assets/images/wedding-pic.jpg'
+  }]
   constructor() { }
 
   ngOnInit() {
