a3d24936a49159802b31f529f1f8d16517af702e
Nick Porter <worldofnick@users.noreply.github.com>
2018-01-31 15:21:51 -0700

    type script model

---
+++ src/app/models/payment.ts
@@ -0,0 +1,11 @@
+import { User } from 'app/models/user';
+import { Event } from 'app/models/event';
+
+export class Payment {
+  public _id: string
+  public hostUser: User
+  public performerUser: User
+  public amount: number
+  public stripeChargeId: string
+  public type: string // {"payment", "refund"}
+}
