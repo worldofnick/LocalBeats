a5418e91a9eef8937050d3e98094ef472ae450bb
Nick Porter <worldofnick@users.noreply.github.com>
2018-01-31 15:52:54 -0700

    handle error

--- src/app/services/payments/stripe.server.ts
+++ src/app/services/payments/stripe.server.ts
@@ -54,4 +54,11 @@ export class StripeService {
 
   }
 
+  private handleError(error: any): Promise<any> {
+      let errMsg = (error.message) ? error.message :
+          error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+      console.error(errMsg); // log to console
+      return Promise.reject(errMsg);
+  }
+
 }
