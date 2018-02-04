6fa43cadf9af3e425bbd204a9ca0b90b36311516
ash-19 <u0946268@utah.edu>
2018-02-04 16:09:10 -0700

    Added working recipient form with dummy autocomplete

--- src/app/views/app-chats/app-chats.module.ts
+++ src/app/views/app-chats/app-chats.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatIconModule, MatButtonModule, MatSidenavModule, MatMenuModule, MatInputModule,
-         MatListModule, MatToolbarModule, MatCardModule } from '@angular/material';
+         MatListModule, MatToolbarModule, MatCardModule, MatAutocompleteModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppChatsComponent } from './app-chats.component';
 import { ChatsRoutes } from "./app-chats.routing";
@@ -21,6 +21,8 @@ import { ChatsRoutes } from "./app-chats.routing";
     MatCardModule,
     FlexLayoutModule,
     FormsModule,
+    ReactiveFormsModule,
+    MatAutocompleteModule,
     RouterModule.forChild(ChatsRoutes)
   ],
   declarations: [AppChatsComponent]
