87c20d3d9a76e839adb1b1c58e5b66e5c6087fee
ash-19 <u0946268@utah.edu>
2018-02-05 14:04:48 -0700

    [BUGFIX] PM snack pops up universally now & not to sender.
    - Added it to home component

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { SocketService } from 'app/services/chats/socket.service';
+import { SocketEvent } from 'app/services/chats/model/event';
+import { Message } from 'app/services/chats/model/message';
+import { MatSnackBar } from '@angular/material';
+import { UserService } from 'app/services/auth/user.service';
 
 @Component({
   selector: 'app-home',
@@ -27,9 +32,28 @@ export class HomeComponent implements OnInit {
     name: 'Featured Wedding',
     url: 'assets/images/wedding-pic.jpg'
   }]
-  constructor() { }
+  constructor(private _userService: UserService, public snackBar: MatSnackBar, private _socketService: SocketService) { 
+    
+  }
 
   ngOnInit() {
+    this.initIoConnection();            // Listen for any private messages
+  }
+
+  openNewMessageSnackBar(message: Message) {
+    if (this._userService.user._id !== message.from._id) {
+      this.snackBar.open('You have a new message from ' + message.from.firstName + ' ' + message.from.lastName,
+                        'close', { duration: 3500 });
+    };
+  }
+
+  initIoConnection() {
+    this._socketService.onEvent(SocketEvent.SEND_PRIVATE_MSG)
+      .subscribe((message: Message) => {
+        console.log('PM from server (main app module): ', message);
+        const temp: Message = message as Message;
+        this.openNewMessageSnackBar(temp);
+    });
   }
 
 }
