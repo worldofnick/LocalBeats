2655086d73cad7175f53699fc604fc40cbfc8b55
Nick Porter <worldofnick@users.noreply.github.com>
2018-02-16 14:06:30 -0700

    Add payment model

---
+++ src/app/models/payment.ts
@@ -0,0 +1,13 @@
+import { User } from 'app/models/user';
+import { Booking } from 'app/models/booking';
+
+export class Payment {
+  public _id: string
+  public hostUser: User
+  public performerUser: User
+  public booking: Booking
+  public date: Date
+  public amount: number
+  public stripeChargeId: string
+  public type: string // {"payment", "refund"}
+}
