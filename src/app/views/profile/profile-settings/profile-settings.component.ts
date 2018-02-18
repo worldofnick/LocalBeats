e5b8518759ead42e29517c6b54a06b0c728a16ee
Nick Porter <worldofnick@users.noreply.github.com>
2018-02-17 21:15:14 -0700

    Update

--- src/app/views/profile/profile-settings/profile-settings.component.ts
+++ src/app/views/profile/profile-settings/profile-settings.component.ts
@@ -9,8 +9,11 @@ import { ImgurService } from 'app/services/image/imgur.service';
 import { MatTabChangeEvent } from '@angular/material';
 import { FormGroup, FormControl, Validators, FormArray, FormBuilder } from '@angular/forms';
 import { SocketService } from '../../../services/chats/socket.service';
+import { StripeService } from '../../../services/payments/stripe.service';
 import { Action } from '../../../services/chats/model/action';
 import { Message } from '../../../services/chats/model/message';
+import { MatSnackBar } from '@angular/material';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile-settings',
@@ -27,9 +30,12 @@ export class ProfileSettingsComponent implements OnInit {
   nowArtist = false;
   public uploader: FileUploader = new FileUploader({ url: 'upload_url' });
   public hasBaseDropZoneOver: boolean = false;
-  constructor(private router: ActivatedRoute, private userService: UserService, 
+  constructor(private route: ActivatedRoute, private userService: UserService, private router : Router,
               private imgurService: ImgurService, private formBuilder: FormBuilder,
-              private _socketService: SocketService) { }
+              private _socketService: SocketService, public snackBar: MatSnackBar, private stripeService: StripeService) { 
+
+                this.showSnackBarIfNeeded();
+              }
 
 
   ngOnInit() {
@@ -60,6 +66,29 @@ export class ProfileSettingsComponent implements OnInit {
   onArtist(){
     this.nowArtist = !this.nowArtist;
   }
+
+  private showSnackBarIfNeeded() {
+    if (this.router.url.indexOf('success=true') >= 0) {
+      let snackBarRef = this.snackBar.open('Stripe Account Linked!', "", {
+        duration: 1500,
+      });
+    } else if (this.router.url.indexOf('success=false') >= 0) {
+      // failure
+      let snackBarRef = this.snackBar.open("Failed to Link Account", "", {
+        duration: 1500,
+      });
+    } else if (this.router.url.indexOf('updated=true') >= 0) {
+      // failure
+      let snackBarRef = this.snackBar.open("Stripe Details Updated", "", {
+        duration: 1500,
+      });
+    } else if (this.router.url.indexOf('updated=false') >= 0) {
+      // failure
+      let snackBarRef = this.snackBar.open("Failed to Update Stripe Details", "", {
+        duration: 1500,
+      });
+  }
+}
 
   public fileOverBase(e: any): void {
     this.hasBaseDropZoneOver = e;
@@ -110,6 +139,46 @@ export class ProfileSettingsComponent implements OnInit {
           this.progressBar.mode = 'determinate';
           //this.router.navigate(['/profile']); //this will go back to my events.
       });
+  }
+
+   // STRIPE
+   authorizeStripe() {
+    this.stripeService.authorizeStripe(this.user).then((url: string) => {
+      window.location.href = url;
+    });
+
+  }
+
+  viewStripeTransfers() {
+    this.stripeService.authorizeStripe(this.user).then((url: string) => {
+      window.open(url);
+    });
+  }
+
+  requestPayout() {
+    this.stripeService.payoutUser(this.user).then((success: boolean) => {
+      if (success) {
+        let snackBarRef = this.snackBar.open('Payout Request Successful!', "", {
+          duration: 1500,
+        });
+      } else {
+        let snackBarRef = this.snackBar.open('Payout Request Failed.', "", {
+          duration: 1500,
+        });
+      }
+    });
+  }
+
+  unlinkStripe() {
+    console.log("unlink stripe");
+    this.user.stripeAccountId = null;
+    this.userService.onEditProfile(this.user).then((user: User) => {
+      this.user = user;
+      this.userService.user = this.user
+      let snackBarRef = this.snackBar.open('Stripe Account Unlinked', "", {
+        duration: 1500,
+      });
+    });
   }
 
 }
