3174a553c3ae42c65a01d0c7254961fba37e5387
Nick Porter <worldofnick@users.noreply.github.com>
2018-02-17 21:19:26 -0700

    Add stripe routes

---
+++ backend/routes/stripeRoutes.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = function(app) {
+	var stripeHandlers = require('../controllers/stripeController.js');
+	var tokenVerificationHandler = require('../controllers/tokenVerificationController.js');
+
+		app.route('/api/stripe/authorize')
+			.post(tokenVerificationHandler.verifyToken, stripeHandlers.stripeAuthorize);
+
+    app.route('/api/stripe/link')
+			.get(stripeHandlers.stripeLink);
+
+		app.route('/api/stripe/transfers')
+			.post(stripeHandlers.stripeTransfers);
+
+		app.route('/api/stripe/payout')
+			.post(stripeHandlers.stripePayout);
+
+		app.route('/api/stripe/charge')
+			.post(stripeHandlers.stripeCharge);
+
+		app.route('/api/stripe/refund')
+			.post(stripeHandlers.stripeRefund);
+
+			app.route('/api/payments/bookingPaymentStatus')
+				.get(stripeHandlers.bookingPaymentStatus);
+
+};
