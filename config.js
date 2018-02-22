964bfa7a878a4e0f39cd702d823d54ff40dcca74
ash-19 <u0946268@utah.edu>
2018-02-22 14:18:37 -0700

    Added JWT_SECRET

--- config.js
+++ config.js
@@ -3,6 +3,8 @@
  * database: the URI with username and password to your MongoDB installation
  */
 module.exports = {
+
+    JWT_SECRET: 'dsfh387hjfVQWxpsSMs8jsbjlJKHSJHD7b2jSY2bBSJKH01dhsbkSCH',
     'secret': 'pikachuineedyou',
     'database': 'mongodb://heroku_dmq914qm:v52suq480nk5qf4m6fh7p5v901@ds147265.mlab.com:47265/heroku_dmq914qm',
     'spotifyClientID' : '9b266aeaa5904de699d2864591f4e248',
