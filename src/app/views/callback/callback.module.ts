59b239397d3d8044f527f66df90f005bd510923d
ash-19 <u0946268@utah.edu>
2018-02-27 16:44:04 -0700

    Added a new callback module

---
+++ src/app/views/callback/callback.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule, MatButtonModule, MatSidenavModule, MatMenuModule, MatInputModule,
+         MatListModule, MatToolbarModule, MatCardModule, MatAutocompleteModule, MatChipsModule, MatDialogModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+//TODO: change
+import { CallbackComponent } from './callback.component';
+import { CallbackRoutes } from "./callback.routing";
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MatSidenavModule,
+    MatMenuModule,
+    MatInputModule,
+    MatIconModule,
+    MatChipsModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatListModule,
+    MatToolbarModule,
+    MatCardModule,
+    FlexLayoutModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatAutocompleteModule,
+    RouterModule.forChild(CallbackRoutes) //TODO: change
+  ],
+  entryComponents: [CallbackComponent],
+  declarations: [CallbackComponent]
+})
+export class CallbackModule { }
