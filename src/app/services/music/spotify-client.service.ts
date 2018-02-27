9a00129f268f15774f168952eb56064775dfe192
ash-19 <u0946268@utah.edu>
2018-02-27 16:06:28 -0700

    Added step 1-3 of spotify authorization to service
    - Redirects to its website, asks user for permission and returns the
    `code` attached to the redirect URL added to the API callback routes

---
+++ src/app/services/music/spotify-client.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions, Response, URLSearchParams } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import { SocketService } from '../../services/chats/socket.service';
+import { User } from 'app/models/user';
+import { SocketEvent } from '../../services/chats/model/event'
+import { environment } from '../../../environments/environment';
+
+
+@Injectable()
+export class SpotifyClientService {
+
+  public authorizeConnection: string = environment.apiURL + 'api/spotify'; 
+  private headers: Headers = new Headers({ 'Content-Type': 'application/json' });
+
+  constructor(private http: Http, private _socketService: SocketService) { }
+
+  // Redirect user to stripe website
+  public authorizeStripe(): Promise<string> {
+    const current = this.authorizeConnection + '/authorize';
+    console.log('Spotify AUth server url: ', current);
+    return this.http.post(current, { headers: this.headers })
+        .toPromise()
+        .then((response: Response) => {
+          const data = response.json();
+          console.log('Data SPotift: ', data);
+          return data.redirect_url;
+        })
+        .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    let errMsg = (error.message) ? error.message :
+        error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+    console.error(errMsg); // log to console
+    return Promise.reject(errMsg);
+}
+}
