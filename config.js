ad7b07526becaa5dda39f9be3a9238f454bd5a7c
ash-19 <u0946268@utah.edu>
2018-02-28 13:24:09 -0700

    Temp locahost redirect for spotify (for testing)

--- config.js
+++ config.js
@@ -13,7 +13,8 @@ module.exports = {
       clientSecret : 'ced120445fe74bdca660090161898476',
       authorizeUri: 'https://accounts.spotify.com/authorize',
       tokenUri: 'https://accounts.spotify.com/api/token',
-      redirectUri : 'https://localbeats.herokuapp.com/callback/spotify'  //TODO: CHANGE_ROUTE change to heroku.localbeats
+      redirectUri : 'http://localhost:4200/callback/spotify'  //TODO: CHANGE_ROUTE change to heroku.localbeats
+      //https://localbeats.herokuapp.com/callback/spotify
     },
     // Configuration for Stripe.
     stripe: {
