648684b8357a419092892e1835d470052ef49aa5
ash-19 <u0946268@utah.edu>
2018-02-27 17:43:11 -0700

    Callback now extract spotify AuthCode

--- src/app/views/callback/callback.component.ts
+++ src/app/views/callback/callback.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+import { SpotifyClientService } from '../../services/music/spotify-client.service';
+import { UserService } from '../../services/auth/user.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-callback',
@@ -7,9 +12,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CallbackComponent implements OnInit {
 
-  constructor() { }
+  spotifyCode: string;
+
+  constructor(private route: ActivatedRoute, private userService: UserService, private router : Router,
+              private _spotifyClientService: SpotifyClientService) { }
 
   ngOnInit() {
+    this.spotifyCode = this.extractSpotifyCode();
+  }
+
+  extractSpotifyCode(): string {
+    const callbackURL = window.location.href;
+    console.log('URL: ', callbackURL);
+    if (callbackURL.indexOf('spotify') >= 0 ) {
+      const typeIndex = callbackURL.indexOf('spotify');
+      console.log('Index: ', typeIndex);
+      if(callbackURL.indexOf('?code=') >= 0 ) {
+        const codeStartIndex = callbackURL.indexOf('?code=');
+        const code = callbackURL.substr(codeStartIndex+6);
+        console.log("Code: ", code);
+      } else if (callbackURL.indexOf('?error=') >= 0 ) {
+        //TODO: no code, handle error (or just redirect to the profile/setting)
+        this.router.navigate(['/profile', 'settings']);
+      }
+    }
   }
 
 }
