404b96bbd466adb33ad40c83bd5a457881911091
ash-19 <u0946268@utah.edu>
2018-03-02 15:11:19 -0700

    Revert "Updated auth guard"
    This reverts commit a02fa5e6f93d7796e7839f08de3666ae3252d790.

--- src/app/services/auth/auth.guard.ts
+++ src/app/services/auth/auth.guard.ts
@@ -1,21 +1,17 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
-import { Observable } from 'rxjs/Observable';
-import { UserService } from './user.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-
-  constructor(private router: Router, private _userService: UserService) {}
-
-
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    if (!this._userService.isAuthenticated()) {
-      this.router.navigate(['/home']);            // TODO: can change it to sign in screen
+  public authToken;
+  private isAuthenticated = true; // Set this value dynamically
+  
+  constructor(private router: Router) {}
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    if (this.isAuthenticated) {
       return false;
     }
-    return true;
+    this.router.navigate(['/sessions/signin']);
+    return false;
   }
 }
