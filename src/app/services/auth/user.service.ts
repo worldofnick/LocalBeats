67ed1c682ef96831cf1830783feee02b805abdf7
ash-19 <u0946268@utah.edu>
2018-03-02 14:53:21 -0700

    Implemented persistence again in a new way

--- src/app/services/auth/user.service.ts
+++ src/app/services/auth/user.service.ts
@@ -1,5 +1,6 @@
 // 'use strict';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Http, Headers, RequestOptions, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import { User } from 'app/models/user';
@@ -10,6 +11,8 @@ import { SocketEvent } from '../../services/chats/model/event';
 import { Action } from '../../services/chats/model/action';
 import { environment } from '../../../environments/environment';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { JwtHelper } from 'angular2-jwt';
+
 // For Angular 5 HttpClient Module
 const httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -26,45 +29,41 @@ export class UserService {
     ioConnection: any;
     action = Action;
 
-    // Authentication Properties
+    // ================================================
+    // Session Persistence
+    // ================================================
+    
+    // Authentication Persistence Properties
+    jwtHelper: JwtHelper = new JwtHelper();
     loggedIn: boolean;
-    public accessToken: string = null;
-    public user: User = null;
+    loggedIn$ = new BehaviorSubject<boolean>(this.loggedIn);
+    public user: User = undefined;
 
     /**
-     * Checks the user session and sets/deletes it. 
+     * Checks the user session and sets/deletes it.
      * Initializes the socket event listeners.
      * @param http To be deprecated
-     * @param _socketService 
-     * @param _httpClient Angular 5 
+     * @param _socketService
+     * @param _httpClient Angular 5
      */
     constructor(private http: Http, private _socketService: SocketService, private _httpClient: HttpClient) {
-        // See if the user has an active session and load it.
-        if (this.isAuthenticated()) {
-            const token = localStorage.getItem('jwtToken');
-            const user: User = JSON.parse(localStorage.getItem('loggedInUser'));
-            
-            this._setSession(token, user);
-            this.getNotificationsCountForUser(user._id);
-            this.getNotificationsForUser(user._id);
-        } else {
-            this._deleteSession(null);
-        }
 
-        // Initial socket event listeners
-        this.initIoConnection();
+        // If authenticated, set local user property and update login status subject
+        // If token is expired, log out to clear any data from localStorage
+        if (this.isAuthenticated()) {
+            this.user = JSON.parse(localStorage.getItem('loggedInUser'));
+            this.setLoggedIn(true);
+            this.initIoConnection();    // Initial socket event listeners
+            // TODO: add notification stuff
+        } else {
+            this.logout();
+        }
     }
 
-    // ================================================
-    // Session Persistence methods
-    // ================================================
-
-    /**
-     * Update login status subject
-     * @param value true or false (logged in or not)
-     */
-    setLoggedIn(value: boolean) {
-        this.loggedIn = value;
+    public setUser(newUser: User) {
+        localStorage.setItem('loggedInUser', JSON.stringify(newUser));
+        // Acts as a validation sanity check
+        this.user = JSON.parse(localStorage.getItem('loggedInUser'));
     }
 
     /**
@@ -73,128 +72,91 @@ export class UserService {
      * notifies the server that a user has logged in
      */
     private _setSession(jwtAccessToken, user) {
-        console.log('JWT Access Token: ', jwtAccessToken);
-        console.log('Logged in user: ', user);
+        if (jwtAccessToken !== null || jwtAccessToken !== undefined) {
+            console.log('JWT Access Token: ', jwtAccessToken);
+            localStorage.setItem('jwtToken', jwtAccessToken);
+        }
 
-        localStorage.setItem('jwtToken', jwtAccessToken);
+        console.log('Logged in user: ', user);
         localStorage.setItem('loggedInUser', JSON.stringify(user));
         this.user = user;
-        this.accessToken = jwtAccessToken;
-    
-        this.notifyNewUserLoginToServer(user);      //TODO: later move it to signIn() method only for performance issues
         this.setLoggedIn(true);
 
-        // this.getUserByID(user._id).then( (gottenUser: User) => {
-        //     localStorage.setItem('loggedInUser', JSON.stringify(gottenUser));
-        //     this.user = gottenUser;
-        //     this.accessToken = jwtAccessToken;
-    
-        //     this.notifyNewUserLoginToServer(gottenUser);      //TODO: later move it to signIn() method only for performance issues
-        //     this.setLoggedIn(true);
-        // } );
+        this.notifyNewUserLoginToServer(user);
     }
 
     /**
-     * Deletes the token, the user in local storage,
-     * sets the logged in status to false, and
-     * notifies the server that a user has logged out
+     * Update login status subject and variable
+     * @param value true or false (logged in or not)
      */
-    private _deleteSession(user) {
+    setLoggedIn(value: boolean) {
+        this.loggedIn$.next(value);     // Update login status subject
+        this.loggedIn = value;
+    }
+
+    /**
+     * Returns if the user's session is still valid based on the
+     *  JWT expiration date (usually 1 whole day from the time of issue)
+     */
+    public isAuthenticated(): boolean {
+        return this.jwtHelper.isTokenExpired(localStorage.getItem('jwtToken'));
+    }
+
+    /**
+     * Notifies the server that this user is loggin out and
+     *  removes all the localStorage data, resets parameters
+     */
+    public logout() {
+        this.notifyUserLoggedOutToServer(this.user);
+        // Remove tokens and profile and update login status subject
         localStorage.removeItem('jwtToken');
         localStorage.removeItem('loggedInUser');
-        this.user = null;                       //TODO: change to undefined later based on what's preferred by Auth0
-        this.accessToken = null;
-
-        if (user !== null) {
-            this.notifyUserLoggedOutToServer(user); //TODO: later move it to logout() method only for performance issues
-        }
+        this.user = undefined;
         this.setLoggedIn(false);
-    }
-
-    /**
-     * Returns if the user has a valid session or not
-     */
-    public isAuthenticated() {
-        // TODO: Check if current date is greater than expiration and if localSTrage token is not null
-        // const expiresAt = JSON.parse(localStorage.getItem('expires_at'));
-        // return Date.now() < expiresAt;
-        // TODO: improve later so no token is persistently stored but managed by the server
-        const jwtToken = localStorage.getItem('jwtToken');
-        if (jwtToken === null) {
-            return false;
-        } else {
-            // TODO: add waiting to load user object here
-            this.accessToken = jwtToken;
-            return true;
-        }
-    }
-
-    public setUser(newUser: User) {
-        localStorage.setItem('loggedInUser', JSON.stringify(newUser));
-        // Acts a validation check if assigning from local storage
-        this.user = JSON.parse(localStorage.getItem('loggedInUser'));
     }
 
     // ===============================================
     // User REST services
     // ===============================================
 
-    // post("api/auth/passwordChange/:uid')
     public signupUser(newUser: User): Promise<User> {
         const current = this.connection + '/register';
-        console.log(newUser)
+        console.log('Sign Up service received: ', newUser);
         return this.http.post(current, newUser, { headers: this.headers })
             .toPromise()
             .then((response: Response) => {
                 const data = response.json();
-                this.user = data.user as User;
                 this._setSession(data.token, (data.user as User));
-
                 this.notifyServerToAddGreetBot(this.user);
-
-                return this.user;
+                return (data.user as User);
             })
             .catch(this.handleError);
     }
 
-    // post("/api/users/uid")
     public onEditProfile(newUser: User): Promise<User> {
         const current = this.userConnection + '/' + newUser._id;
         return this.http.put(current, { user: newUser }, { headers: this.headers })
             .toPromise()
             .then((response: Response) => {
                 const data = response.json();
-                this.user = data.user as User;
-                this._setSession(this.accessToken, this.user);
-                return this.user
+                this._setSession(undefined, (data.user as User));
+                return (data.user as User);
             })
             .catch(this.handleError);
     }
 
-    // post("/api/authenticate")
     public signinUser(returningUser: User): Promise<User> {
-        // this.connection = 'http://localhost:8080/api/auth/authenticate';
         const current = this.connection + '/authenticate';
-        // console.log('Returning User in auth: ', returningUser);
         return this.http.post(current, returningUser, { headers: this.headers })
             .toPromise()
             .then((response: Response) => {
                 const data = response.json();
-                this.user = data.user as User;
                 this._setSession(data.token, (data.user as User));
-
-                return this.user;
+                return (data.user as User);
             })
             .catch(this.handleError);
     }
 
-    /**
-     * 
-     * @param user 
-     * change password
-     * /api/auth/passwordChange/:uid')
-     */
-    // /api/auth/passwordChange/:uid')
     public updatePassword(user: User): Promise<User> {
         const current = this.connection + '/passwordChange/' + user._id;
         let newPassword: string = user.password;
@@ -202,40 +164,21 @@ export class UserService {
             .toPromise()
             .then((response: Response) => {
                 const data = response.json();
-                this.user = data.user as User;
-                return this.user;
+                this._setSession(undefined, (data.user as User));
+                return (data.user as User);
             })
             .catch(this.handleError);
     }
 
-    // get user
-    ///api/users/:uid
-    /**
-     * 
-     * @param userToGet 
-     */
     public getUserByID(ID: String): Promise<User> {
         const current = this.userConnection + '/' + ID;
         return this.http.get(current)
             .toPromise()
             .then((response: Response) => {
                 const data = response.json();
-                // this.accessToken = data.token;
                 console.log('Got user from server: ', data.user);
-                let temp = data.user as User;
-                return temp;
-            })
-            .catch(this.handleError);
-    }
-
-    public logout() {
-        let from: User = this.user;
-        const current = this.connection + '/logout';
-        console.log('LOGOUT USER: ', this.user);
-        return this.http.post(current, this.user, { headers: this.headers })
-            .toPromise()
-            .then((response: Response) => {
-                this._deleteSession(from);
+                this._setSession(undefined, (data.user as User));
+                return data.user as User;
             })
             .catch(this.handleError);
     }
@@ -290,71 +233,6 @@ export class UserService {
         });
     }
 
-    // ===========================================
-    // Notification Methods
-    // ===========================================
-
-    public getNotificationsCountForUser(ID: any): Promise<Number> {
-        let userConnection: string = environment.apiURL + 'api/notification';
-        // app.route('/api/notification/:uid')
-        const current = userConnection + '/' + ID;
-        // const current = userConnection + '/5a7113ac9d89a873c89fe5ff';
-        //console.log("getting: ");
-        //console.log(current);
-        return this.http.get(current)
-            .toPromise()
-            .then((response: Response) => {
-                const data = response.json();
-                console.log(data)
-                // this.accessToken = data.token;
-                // console.log(this.accessToken)
-                let temp = data.notifications as Notification[];
-                if (temp == null) {
-                    return 0;
-                }
-
-                this._socketService.socket.emit('tellTopBar', temp.length)
-                // this._socketService.send(Action.REQUEST_NOTIFICATION_COUNT, {
-                //     from: 'tellTopBar',
-                //     action: Action.SMN_LOGGED_OUT
-                // });
-
-                return temp.length;
-            })
-            .catch(this.handleError);
-    }
-
-    public getNotificationsForUser(ID: any): Promise<Notification[]> {
-        let userConnection: string = environment.apiURL + 'api/notification';
-        const current = userConnection + '/' + ID;
-        // const current = userConnection + '/5a7113ac9d89a873c89fe5ff';
-
-        //console.log("getting: ");
-        //console.log(current);
-        return this.http.get(current)
-            .toPromise()
-            .then((response: Response) => {
-                const data = response.json();
-                // this.accessToken = data.token;
-                // console.log(this.accessToken)
-                //inserting test notifications until i can actually send them.
-                let temp = data.notifications as Notification[];
-                // console.log(data);
-                let t: Notification[] = [];
-
-                if (temp == null) {
-                    // this.io.emit('tellNotificationPanel', t)
-                    return t;
-                }
-
-                this._socketService.socket.emit('tellNotificationPanel', temp)
-                // this._socketService.socket.emit('tellTopBar', temp.length)
-
-                return temp;
-            })
-            .catch(this.handleError);
-    }
-
     private handleError(error: any): Promise<any> {
         let errMsg = (error.message) ? error.message :
             error.status ? `${error.status} - ${error.statusText}` : 'Server error';
@@ -363,7 +241,7 @@ export class UserService {
     }
 
     // =====================================
-    // Other method (WILL BE CHANGED LATER, DO NOT TOUCH)
+    // Other methods (DO NOT TOUCH)
     // =====================================
 
     private openDialog = function (uri, name, options, cb) {
@@ -389,4 +267,54 @@ export class UserService {
         };
         return parts.join('&');
     };
+
+    // ===========================================
+    // Notification Methods
+    // ===========================================
+
+    public getNotificationsCountForUser(ID: any): Promise<Number> {
+        let userConnection: string = environment.apiURL + 'api/notification';
+        const current = userConnection + '/' + ID;
+        return this.http.get(current)
+            .toPromise()
+            .then((response: Response) => {
+                const data = response.json();
+                console.log(data);
+                let temp = data.notifications as Notification[];
+                if (temp == null) {
+                    return 0;
+                }
+
+                this._socketService.socket.emit('tellTopBar', temp.length);
+                // TODO: Improvement Suggestion: Also create a notification_request_event 
+                // to get notifications and the total count via socket call.
+                return temp.length;
+            })
+            .catch(this.handleError);
+    }
+
+    public getNotificationsForUser(ID: any): Promise<Notification[]> {
+        let userConnection: string = environment.apiURL + 'api/notification';
+        const current = userConnection + '/' + ID;
+        return this.http.get(current)
+            .toPromise()
+            .then((response: Response) => {
+                const data = response.json();
+                //inserting test notifications until i can actually send them.
+                let temp = data.notifications as Notification[];
+                // console.log(data);
+                let t: Notification[] = [];
+
+                if (temp == null) {
+                    // this.io.emit('tellNotificationPanel', t)
+                    return t;
+                }
+
+                this._socketService.socket.emit('tellNotificationPanel', temp)
+                // this._socketService.socket.emit('tellTopBar', temp.length)
+
+                return temp;
+            })
+            .catch(this.handleError);
+    }
 }
