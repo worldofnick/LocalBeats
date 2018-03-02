0d900da33e5e6cf7663cf636e7d146d517d99232
ash-19 <u0946268@utah.edu>
2018-03-02 08:07:30 -0700

    Made user model fields all optional

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -3,32 +3,34 @@ import * as io from 'socket.io';
 
 
 export class User {
-    public _id: string;
-    public firstName: string;
-    public lastName: string;
-    public email: string;
-    public spotifyID: string;       //TODO: remove later. DEPRECATED
-    public password: string;
-    public genres: string[];
-    public isArtist: boolean;
-    public profilePicUrl: string;
-    public eventTypes: string[];
-    public socket: io.socket;
-    public city: string;
-    public state: string;
-    public location: number[];
-    public stripeAccountId: string;
+    public _id?: string;
+    public firstName?: string;
+    public lastName?: string;
+    public email?: string;
+    public spotifyID?: string;       //TODO: remove later. DEPRECATED
+    public password?: string;
+    public genres?: string[];
+    public isArtist?: boolean;
+    public profilePicUrl?: string;
+    public eventTypes?: string[];
+    public socket?: io.socket;
+    public city?: string;
+    public state?: string;
+    public location?: number[];
+    public stripeAccountId?: string;
     
     // Music properties
     public spotify?: {
-        email: string,
-        id: string,
-        uri: string,
-        href: string,
-        albums: any[]
+        email?: string,
+        id?: string,
+        uri?: string,
+        href?: string,
+        accessToken?: string,
+        refreshToken?: string,
+        albums?: any[]
     };
 
     // Chat properties
-    public isOnline: boolean = false;
+    public isOnline?: boolean = false;
 }
 
