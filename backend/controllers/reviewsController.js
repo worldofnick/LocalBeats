1b45fea1cd13bf236f646ef270457d5e90f800d7
Brandon Koch <brandon.gregory.koch@gmail.com>
2018-03-03 23:51:00 -0700

    Reviews controller

--- backend/controllers/reviewsController.js
+++ backend/controllers/reviewsController.js
@@ -9,7 +9,7 @@ var config    = require('../../config.js');
 // ====== Reviews ROUTES ======
 
 exports.listAllReviews = function (req, res) {
-    Reviews.find({}, function (err, reviews) {
+    Reviews.find({}).populate("fromUser").populate("toUser").populate("booking").exec( function (err, reviews) {
       if (err)
         return res.send(err);
             
@@ -18,7 +18,7 @@ exports.listAllReviews = function (req, res) {
 };
 
 exports.getReviewByID = function (req, res) {
-  Reviews.findById(req.params.rid, function (err, review) {
+  Reviews.findById(req.params.rid).populate("fromUser").populate("toUser").populate("booking").exec( function (err, review) {
       if (err) {
           return res.status(500).send("Failed to get review");
       } else {
@@ -36,7 +36,13 @@ exports.createReview = function (req, res) {
                 description: "Failed to create a review"
             });
         } else {
-            return res.status(200).send({ "review": review });
+            Reviews.findById(newReview._id).populate("fromUser").populate("toUser").populate("booking").exec(function (err, review) {
+                if (err) {
+                    return res.status(500).send("Failed to create review");
+                } else {
+                    return res.status(200).send({ "review": review });
+                }
+            });
         }
     });
 };
@@ -46,12 +52,18 @@ exports.updateReviewByID = function (req, res) {
         if (err) {
             return res.status(500).send("There was a problem updating the review.");
         }
-        return res.status(200).send({ "review": review });
+        Reviews.findById(review._id).populate("fromUser").populate("toUser").populate("booking").exec(function (err, review) {
+            if (err) {
+                return res.status(500).send("Failed to update review");
+            } else {
+                return res.status(200).send({ "review": review });
+            }
+        });
     });
 };
 
 exports.deleteReviewByID = function (req, res) {
-    Reviews.findByIdAndRemove(req.params.bid, function (err, review) {
+    Reviews.findByIdAndRemove(req.params.rid, function (err, review) {
         if (err) {
             return res.status(500).send("There was a problem deleting the review.");
         } else {
@@ -76,8 +88,8 @@ exports.getUserReviewsByUIDTo = function (req, res) {
   if (req.params.skip != null) {
       skip = parseInt(req.query.skip);
   }
-
-  Reviews.find({toUID: req.query.uid}).limit(limit).skip(skip).exec(function (err, doc) {
+  
+  Reviews.find({toUser: req.query.uid}).populate("fromUser").populate("toUser").populate("booking").exec(function (err, doc) {
       if (err) {
           return res.status(500).send("Failed to get user reviews");
       } else {
@@ -99,11 +111,12 @@ exports.getUserReviewsByUIDFrom = function (req, res) {
       skip = parseInt(req.query.skip);
   }
 
-  Reviews.find({fromUID: req.query.uid}).limit(limit).skip(skip).exec(function (err, doc) {
+  Reviews.find({fromUser: req.query.uid}).limit(limit).skip(skip).populate("fromUser").populate("toUser").populate("booking").exec(function (err, doc) {
       if (err) {
           return res.status(500).send("Failed to get user reviews");
       } else {
-          return res.status(200).send({"reviews": doc});
+                
+            return res.status(200).send({"reviews": doc});
       }
   });
 };
