19456d1c2d3644db6a8846a8fd575a62cc8f53d9
ash-19 <u0946268@utah.edu>
2018-03-05 09:18:04 -0700

    Added shared service for snack bar message

--- src/app/services/shared/shared-data.service.ts
+++ src/app/services/shared/shared-data.service.ts
@@ -4,11 +4,17 @@ import { User } from '../../models/user';
 @Injectable()
 export class SharedDataService {
 
+  // Profile button chat message
   profileButtonChatRecipient: User = new User();
   isProfileUserRequestPending = false;
 
+  // Snack bar go to chat message button
+  snackBarMessageRecipient: User = new User();
+  isSnackBarMessageRequestPending = false;
+
   constructor() { }
 
+  // Profile button chat message
   public setProfileMessageSharedProperties(recipient: User) {
     console.log('>>> IN SHARED SERVICE SET');
     this.profileButtonChatRecipient = recipient;
@@ -21,4 +27,16 @@ export class SharedDataService {
     this.isProfileUserRequestPending = false;
   }
 
+  // Snack bar go to chat message button
+  public setSnackBarMessageSharedProperties(recipient: User) {
+    console.log('>>> IN SHARED SERVICE SET');
+    this.snackBarMessageRecipient = recipient;
+    this.isSnackBarMessageRequestPending = true;
+  }
+
+  public resetSnackBarMessageSharedProperties() {
+    console.log('>>> IN SHARED SERVICE RESET');
+    this.snackBarMessageRecipient = new User();
+    this.isSnackBarMessageRequestPending = false;
+  }
 }
