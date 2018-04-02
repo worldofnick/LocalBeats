25706fa937c0e2470c6c38587715b356cfcf788f
Nick Porter <worldofnick@users.noreply.github.com>
2018-04-01 21:25:13 -0600

    remove logs

--- src/app/components/common/topbar/topbar.component.ts
+++ src/app/components/common/topbar/topbar.component.ts
@@ -206,10 +206,6 @@ export class TopbarComponent implements OnInit, OnDestroy {
   
   // Submission of search
   submit(expand: boolean = true) {
-
-    console.log("value");
-    console.log(this.searchForm.get('sort').value);
-
     this.currentSearch.from_date = this.searchForm.get('startDate').value;
     this.currentSearch.to_date = this.searchForm.get('endDate').value;
     // Set location for submission
